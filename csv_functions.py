1e7c9d4fc640d8da897c5933375e576fda64ff00
Jeff Butcher <jbutcher@senzing.com>
2020-03-05 12:26:37 -0800

    fix for prefix/suffix on attributes

--- csv_functions.py
+++ csv_functions.py
@@ -14,42 +14,39 @@ class csv_functions():
         self.initialized = True
         self.statPack = {}
 
-        variantFile = __file__.replace('.py', '.json')
-        if not os.path.exists(variantFile):
+        self.variantFile = __file__.replace('.py', '.json')
+        if not os.path.exists(self.variantFile):
             print('')
-            print('File %s is missing!' % variantFile)
+            print('File %s is missing!' % self.variantFile)
             print('')
             self.initialized = False
             return
 
-        try: self.variantData = json.load(open(variantFile,'r', encoding='latin-1'))
+        try: self.variantJson = json.load(open(self.variantFile,'r', encoding='latin-1'))
         except json.decoder.JSONDecodeError as err:
             print('')
-            print('JSON error %s in %s' % (err, variantFile))
+            print('JSON error %s in %s' % (err, self.variantFile))
             print('')
             self.initialized = False
             return
 
+        #--ensure these lists exist
+        if 'GARBAGE_VALUES' not in self.variantJson:
+            self.variantJson['GARBAGE_VALUES'] = []
+        if 'ORGANIZATION_TOKENS' not in self.variantJson:
+            self.variantJson['ORGANIZATION_TOKENS'] = []
+        if 'PERSON_TOKENS' not in self.variantJson:
+            self.variantJson['PERSON_TOKENS'] = []
+        if 'SENZING_ATTRIBUTES' not in self.variantJson:
+            self.variantJson['SENZING_ATTRIBUTES'] = []
+
         #--turn lists into dictionaries for speed
-        if 'GARBAGE_VALUES' not in self.variantData:
-            self.variantData['GARBAGE_VALUES'] = {}
-        else:
-            self.variantData['GARBAGE_VALUES'] = dict(zip(self.variantData['GARBAGE_VALUES'], [''] * len(self.variantData['GARBAGE_VALUES'])))
-
-        if 'ORGANIZATION_TOKENS' not in self.variantData:
-            self.variantData['ORGANIZATION_TOKENS'] = {}
-        else:
-            self.variantData['ORGANIZATION_TOKENS'] = dict(zip(self.variantData['ORGANIZATION_TOKENS'], [''] * len(self.variantData['ORGANIZATION_TOKENS'])))
-
-        if 'PERSON_TOKENS' not in self.variantData:
-            self.variantData['PERSON_TOKENS'] = {}
-        else:
-            self.variantData['PERSON_TOKENS'] = dict(zip(self.variantData['PERSON_TOKENS'], [''] * len(self.variantData['PERSON_TOKENS'])))
-
-        if 'SENZING_ATTRIBUTES' not in self.variantData:
-            self.variantData['SENZING_ATTRIBUTES'] = []
-        else:
-            self.variantData['SENZING_ATTRIBUTES'] = dict(zip(self.variantData['SENZING_ATTRIBUTES'], [''] * len(self.variantData['SENZING_ATTRIBUTES'])))
+        self.variantData = {}
+        for variantList in self.variantJson:
+            if type(self.variantJson[variantList]) == list:
+                self.variantData[variantList] = dict(zip([i.upper() for i in self.variantJson[variantList]], [''] * len(self.variantJson[variantList])))
+            else:
+                self.variantData[variantList] = self.variantJson[variantList]
 
         #--supported date formats
         self.dateFormats = []
@@ -102,21 +99,31 @@ class csv_functions():
         return returnValue
 
     #-----------------------------------
-    def is_senzing_attribute(self, attrString):
-        if attrString in self.variantData['SENZING_ATTRIBUTES']:
+    def is_senzing_attribute(self, attrName):
+        attrName = attrName.upper()
+        if attrName in self.variantData['SENZING_ATTRIBUTES']:
             return True
+        elif '_' in attrName:
+            baseName = attrName[attrName.find('_') + 1:]
+            if baseName in self.variantData['SENZING_ATTRIBUTES']:
+                return True
+            else:
+                baseName = attrName[0:attrName.rfind('_')]
+                if baseName in self.variantData['SENZING_ATTRIBUTES']:
+                    return True
         return False
 
     #-----------------------------------
-    def get_senzing_attribute(self, attrString):
-        if attrString in self.variantData['SENZING_ATTRIBUTES']:
-            return self.variantData['SENZING_ATTRIBUTES'][attrString]
+    def get_senzing_attribute(self, attrName):
+        attrName = attrName.upper()
+        if attrName in self.variantData['SENZING_ATTRIBUTES']:
+            return self.variantData['SENZING_ATTRIBUTES'][attrName]
         return {}
 
     #-----------------------------------
     def is_organization_name(self, nameString):
         if nameString:
-            for token in nameString.lower().replace('.',' ').replace(',',' ').split():
+            for token in nameString.replace('.',' ').replace(',',' ').split():
                 if token.upper() in self.variantData['ORGANIZATION_TOKENS']:
                     return True
         return False
@@ -124,7 +131,7 @@ class csv_functions():
     #-----------------------------------
     def is_person_name(self, nameString):
         if nameString:
-            for token in nameString.lower().replace('.',' ').replace(',',' ').split():
+            for token in nameString.replace('.',' ').replace(',',' ').split():
                 if token.upper() in self.variantData['PERSON_TOKENS']:
                     return True
         return False
@@ -133,18 +140,107 @@ class csv_functions():
 if __name__ == "__main__":
     appPath = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-    global shutDown
-    shutDown = False
-    signal.signal(signal.SIGINT, signal_handler)
-    procStartTime = time.time()
-    progressInterval = 10000
 
     #--test the instance
     csvFunctions = csv_functions()
-    if csvFunctions.initialized:
-        print('')
-        print('successfully initialized!')
-        print('')
+    if not csvFunctions.initialized:
+        sys.exit(1)
 
-    sys.exit()
+    #--see if they entered arguments
+    argStr = ' '.join(sys.argv[1:])
+    if len(sys.argv) == 1 or 'help' in argStr.lower() or '-h' in argStr.lower():
+        print()
+        print('possible commands ...')
+        print('\tdisplay lists')
+        print('\tadd <token> to <list>')
+        #print('\tdelete token from list')
+        print('\tadd file asdf.txt to list')
+        print()
+        sys.exit(0)
 
+    if argStr.lower() == 'display lists':
+        print()
+        for key in csvFunctions.variantJson.keys():
+            print (key)
+        print()
+        sys.exit(0)
+
+    if sys.argv[1].lower() == 'add' and len(sys.argv) >= 5: 
+        variantTokens = []
+        fileName = ''
+        targetList = ''
+        priorToken = ''
+        for token in sys.argv[2:]:
+            if priorToken == 'file':
+                fileName = token
+            elif priorToken == 'to':
+                targetList = token
+            elif token.lower() not in ('file', 'to'):
+                variantTokens.append(token)
+            priorToken = token.lower()
+
+        variant = ' '.join(variantTokens).upper()
+        targetList = targetList.upper()
+        errorText = ''
+
+        if targetList not in csvFunctions.variantJson:
+            print()
+            ok = input('%s is not a current list, create it? (Y/n) ' % targetList)
+            if ok.upper().startswith('Y'):
+                csvFunctions.variantJson[targetList] = []
+            else:
+                errorText = 'aborted!'
+        elif fileName and variant:
+            #print ('fileName = %s' % fileName)
+            #print ('variant = %s' % variant)
+            errorText = 'command not recognized'
+        elif fileName and not os.path.exists(fileName):
+            errorText = '%s not found' % fileName
+
+        if errorText:
+            print()
+            print(errorText)
+            print()
+            sys.exit(1)
+        else:
+
+            print()
+            addCnt = 0
+            skipCnt = 0
+            if fileName:
+                variantList = [line. rstrip('\n') for line in open(fileName)]
+            else:
+                variantList = [variant]
+            for variant in variantList:
+                variant = variant.upper()
+                if variant in csvFunctions.variantJson[targetList]:
+                    print('exists: %s' % variant)
+                    skipCnt += 1
+                else:
+                    csvFunctions.variantJson[targetList].append(variant)
+                    print('added: %s' % variant)
+                    addCnt += 1
+            print()
+            if addCnt > 0:
+                print()
+                ok = input('ok to save? (Y/n) ')
+                if ok.upper().startswith('Y'):
+
+                    #--ensures lists are sorted
+                    for variantList in csvFunctions.variantJson:
+                        if type(csvFunctions.variantJson[variantList]) == list:
+                            csvFunctions.variantJson[variantList] = sorted(csvFunctions.variantJson[variantList])
+                    #--write to file
+                    with open(csvFunctions.variantFile, 'w') as f:
+                        json.dump(csvFunctions.variantJson, f, indent=4, sort_keys = True)
+                    print()
+                    print('saved!')
+                    print()
+
+            sys.exit(0)
+
+
+    print()
+    print('command not recognized')
+    print()
+    sys.exit(1)
